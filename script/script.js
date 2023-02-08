1536b2a320b3539166f96fa53c1f05b9132b49b5
BigZetena <v.o.sharipov@gmail.com>
2023-02-08 23:40:58 +0300

    add buttons & refactoring

--- script/script.js
+++ script/script.js
@@ -2,51 +2,83 @@ const ul = document.querySelector("ul");
 const input = document.querySelector("input");
 const addButton = document.querySelector(".book__add");
 const allDeleteButton = document.querySelector(".book__deleteAll");
-const noteData = [];
+const checkedDeleteButton = document.querySelector(".book__deleteReady");
+let noteData = [];
 
 function addNoteToData() {
   if (input.value.trim().length) {
-    noteData.push(input.value);
-    createNote();
+    noteData = [
+      ...noteData,
+      {
+        note: input.value,
+        id: new Date().toISOString(),
+        checked: false,
+      },
+    ];
+    createNote(noteData);
+    drawTodos(noteData);
   }
 }
 
-function createNote() {
-  let noteId = noteData.length - 1;
-  let checkboxId = noteId + "box";
-  let deleteId = noteId + "delete";
+function createNote(obj) {
   const noteItem = document.createElement("li");
   noteItem.className = "list__item";
-  noteItem.id =
-    noteItem.innerHTML = `<input type="checkbox" class="list__checkbox" id="${checkboxId}"/>
-                         <p class="list__text" id="${noteId}">${noteData[noteId]}</p>
-                         <button class="list__button" id="${deleteId}">❌</button>`;
-  ul.append(noteItem);
-  console.log(noteData);
-  const listDeleteButton = document.getElementById(deleteId);
-  listDeleteButton.addEventListener("click", function deleteNote() {
-    ul.removeChild(noteItem);
-    noteData.splice(noteId, 1);
-  });
-  const listCheck = document.getElementById(checkboxId);
-  listCheck.addEventListener("click", function checkedNote() {
-    const noteText = document.getElementById(noteId);
-    if (listCheck.checked) {
-      noteText.className = "list__text__checked list__text";
-    } else {
-      noteText.className = "list__text";
-    }
-    console.log("hui");
-  });
-}
 
-function allDelete() {
-  const liList = document.querySelector(".list__item");
-  for (let k = noteData.length; k > 0; k - 1) {
-    ul.removeChild(liList);
-    noteData.splice(noteId, 1);
+  const listCheck = document.createElement("input");
+  listCheck.type = "checkbox";
+  listCheck.className = "list__checkbox";
+  if (obj.checked) listCheck.checked = true;
+  noteItem.append(listCheck);
+
+  const listText = document.createElement("p");
+  listText.className = "list__text";
+  listText.textContent = obj.note;
+  noteItem.append(listText);
+
+  const listDeleteButton = document.createElement("button");
+  listDeleteButton.className = ".list__button";
+  listDeleteButton.textContent = "X";
+  noteItem.append(listDeleteButton);
+
+  listDeleteButton.addEventListener("click", deleteNote);
+
+  function deleteNote() {
+    noteData = noteData.filter((item) => item.id !== obj.id);
+    drawTodos(noteData);
   }
+
+  listCheck.addEventListener("click", checkedNote);
+
+  function checkedNote() {
+    noteData.forEach((item) =>
+      item.id == obj.id
+        ? (item.checked = !item.checked)
+        : (item.checked = item.checked)
+    );
+  }
+
+  return noteItem;
 }
 
-allDeleteButton.addEventListener("click", allDelete);
+function drawTodos(data) {
+  ul.innerHTML = "";
+  data.forEach((item) => {
+    ul.append(createNote(item));
+  });
+}
+
+function deleteAll() {
+  noteData = [];
+  drawTodos(noteData);
+}
+
+function deleteChecked() {
+  noteData = noteData.filter((item) => item.checked == false);
+  drawTodos(noteData);
+}
+
 addButton.addEventListener("click", addNoteToData);
+
+allDeleteButton.addEventListener("click", deleteAll);
+
+checkedDeleteButton.addEventListener("click", deleteChecked);
